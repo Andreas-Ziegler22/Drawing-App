40339669de421012f7b9251ac0c49d064b43a238
MrBurp <andreass.ziegler@gmail.com>
2022-10-01 22:09:31 +0200

    drawLine working

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ const ctx = canvas.getContext("2d");
 let size = 20;
 let color = "black";
 
+let x; // main axis x
+let y; // cross axis y
+
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
   ctx.fillStyle = color;
   ctx.fill();
 }
-drawCircle(100, 200);
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size;
+  ctx.stroke();
+}
